3449f34cf8b05d15727d7b235c8a3b996a4ac3ea
lowlyHero <e.nunn1009@gmail.com>
2019-10-17 17:48:15 -0400

    Trying to display res msg on site deploy

--- api/server.js
+++ api/server.js
@@ -5,8 +5,4 @@ const helmet = require('helmet');
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send('Welcome to the Spider-Man PS4 Twitter API!');
-});
-
 module.exports = server;
--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const port = process.env.PORT || 3300;
 server.listen(port, () => {
   console.log(`\n=== Listening on port ${port}\n`);
 });
+
+server.get('/', (req, res) => {
+    res.send('Welcome to the Spider-Man PS4 Twitter API!');
+});
